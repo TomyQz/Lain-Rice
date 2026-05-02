03b9aa7d706426f48c52649a85b2194dc48e3cf0
TomyQz <josetomascornejo0@gmail.com>
2026-05-02 14:57:41 -0400

    Dotfiles and more

---
+++ Dotfiles/hypr/wired.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script para el efecto de texto de Lain
+while true; do
+    clear
+    echo "Ｃｏｎｎｅｃｔｅｄ　ｔｏ　ｔｈｅ　Ｗｉｒｅｄ．．．"
+    sleep 3
+    clear
+    echo "Ｌｅｔ'ｓ　ａｌｌ　ｌｏｖｅ　Ｌａｉｎ"
+    sleep 3
+    clear
+    echo "Ｇｏｄ　ｉｓ　ｈｅｒｅ　ｉｎ　ｔｈｅ　Ｗｉｒｅｄ"
+    sleep 3
+done
